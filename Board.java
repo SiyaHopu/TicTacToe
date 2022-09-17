a81badd0ebf1ec9a67b18d5520029783381228a9
SiyaHopu <113137595+SiyaHopu@users.noreply.github.com>
2022-09-17 16:30:22 +0200

    Update Board.java

--- Board.java
+++ Board.java
@@ -5,11 +5,14 @@ import java.awt.*;
 public class Board{
   
   JFrame gameBoard;
+  // Constuctor
   
   Board(){
     
+    // Initiates the game board
     gameBoard =  new JFrame();
     
+    // Sets the 3x3 grid layout.
     gameBoard.setLayout(new GridLayout(rows,cols));
     gameboard.setSize(300,300);
     gameBoard.setVisible(true);
@@ -17,6 +20,9 @@ public class Board{
   }
     
   public static void main(String [] args){
+    
+    int rows = 3;
+    int cols = 3;
     
     new Board();
     
