a10be8559595919c9521e8613a66f1f62882dc09
SiyaHopu <113137595+SiyaHopu@users.noreply.github.com>
2022-10-11 20:52:46 +0200

    Update State.java

--- State.java
+++ State.java
@@ -1,1 +1,21 @@
+import java.awt.*;
+import java.util.*;
+import java.awt.event.*;
+import java.swing.*;
 
+
+public class State implements ActionListener{
+	
+	JFrame frame = new JFrame();
+	JPanel title = new JPanel();
+	JLabel textL = new JLabel)();
+	public void fistTurn(){
+
+	boolean player1_turn, player2_turn;
+
+	} 
+	public void xWins(){
+	} 
+	public void check(){
+	} 
+} 
